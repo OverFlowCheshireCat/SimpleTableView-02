0d4f244915db60b50dd978390f628d169e264c64
dit03 <dit@imac-4.local>
2019-05-02 16:16:31 +0900

    Last Source

--- SimpleTableView 02/ViewController.swift
+++ SimpleTableView 02/ViewController.swift
@@ -4,8 +4,28 @@ class ViewController: UIViewController, UITableViewDataSource {
     
     @IBOutlet weak var tableView: UITableView!
     
-    var animals = ["Dog", "Cat", "Cow", "Pig", "Horse", "Whale"]
-    var ages = ["1", "2", "3", "4", "5", "6"]
+    var animals = ["Bird", "Cat", "Pig", "Horse", "Whale"]
+    var ages = ["1 세", "2 세", "3 세", "4 세", "5 세"]
+    
+    public func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        if section == 0 {
+            return "Animal Farm 1"
+        } else {
+            return "Animal Farm 2"
+        }
+    }
+    
+    public func tableView(_ tableView: UITableView, titleForFooterInSection section: Int) -> String? {
+        if section == 0 {
+            return "End Of Animal Farm 1"
+        } else {
+            return "End Of Animal Farm 2"
+        }
+    }
+    
+    public func numberOfSections(in tableView: UITableView) -> Int {
+        return 2
+    }
     
     public func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return animals.count
@@ -17,6 +37,7 @@ class ViewController: UIViewController, UITableViewDataSource {
         
         cell.textLabel?.text = animals[row]
         cell.detailTextLabel?.text = ages[row]
+        cell.imageView?.image = UIImage(named: animals[row])
         
         return cell
     }
