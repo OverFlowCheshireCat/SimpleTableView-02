007c8de57adda1ea7c16d38d8a780cf839ca6d0e
dit03 <dit@imac-4.local>
2019-05-02 15:29:26 +0900

    Edit Source

--- SimpleTableView 02/ViewController.swift
+++ SimpleTableView 02/ViewController.swift
@@ -1,20 +1,29 @@
-//
-//  ViewController.swift
-//  SimpleTableView 02
-//
-//  Created by dit03 on 2019. 5. 2..
-//  Copyright © 2019년 201730529 김민철. All rights reserved.
-//
-
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UITableViewDataSource {
+    
+    @IBOutlet weak var tableView: UITableView!
+    
+    var animals = ["Dog", "Cat", "Cow", "Pig", "Horse", "Whale"]
+    var ages = ["1", "2", "3", "4", "5", "6"]
+    
+    public func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return animals.count
+    }
+    
+    public func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Re", for: indexPath)
+        let row = indexPath.row
+        
+        cell.textLabel?.text = animals[row]
+        cell.detailTextLabel?.text = ages[row]
+        
+        return cell
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        tableView.dataSource = self
     }
-
-
 }
-
